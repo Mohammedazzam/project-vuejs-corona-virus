3465bdfdd4cdcd4195879f53569a16c83ae2a9a4
Mohammed Azzam <mohammed.mojahed93@gmail.com>
2021-03-23 00:55:04 +0200

    (install vue-router)

--- src/main.js
+++ src/main.js
@@ -1,4 +1,16 @@
 import { createApp } from 'vue'
+import { createRouter, createWebHistory } from 'vue-router'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+const router = createRouter({
+    history: createWebHistory(),
+    routes: [],
+})
+
+const app = createApp(App)
+app.use(router)
+
+router.isReady().then(() => {
+    app.mount('#app')
+})
+
